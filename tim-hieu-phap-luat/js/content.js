746ddc6db03d334160890a75d5b4e7e54420ae3d
Hoang-Dat <dat01655993202@gmail.com>
2024-09-12 00:28:30 +0700

    update

--- tim-hieu-phap-luat/js/content.js
+++ tim-hieu-phap-luat/js/content.js
@@ -9,6 +9,15 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
                 return false;
             }
         })
-        alert(ketqua?.result);
+        if (ketqua?.result) {
+            let htmlReplace = `<span style="color: #5dab2d; background-color: yellow;">${ketqua?.result}</span>`;
+            $('*:contains("'+ketqua.result+'")').each((inde, el) => {
+                let text = el.innerHTML;
+                text = text.replace(new RegExp(ketqua.result, 'gmi'), htmlReplace);
+                el.innerHTML = text;
+            })
+        } else {
+            alert('Không có kết quả');
+        }
     }
   });
