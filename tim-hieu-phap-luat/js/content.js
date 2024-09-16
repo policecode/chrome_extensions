e53037e03f1a3bad02f7e6d1fb0803872bda1523
Nguyễn Hoàng Đạt <dat01655993202@gmail.com>
2024-09-16 20:51:05 +0700

    update

--- tim-hieu-phap-luat/js/content.js
+++ tim-hieu-phap-luat/js/content.js
@@ -10,12 +10,13 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
             }
         })
         if (ketqua?.result) {
-            let htmlReplace = `<span style="color: #5dab2d; background-color: yellow;">${ketqua?.result}</span>`;
-            $('*:contains("'+ketqua.result+'")').each((inde, el) => {
-                let text = el.innerHTML;
-                text = text.replace(new RegExp(ketqua.result, 'gmi'), htmlReplace);
-                el.innerHTML = text;
-            })
+            // let htmlReplace = `<span style="color: #5dab2d; background-color: yellow;">${ketqua?.result}</span>`;
+            // $('*:contains("'+ketqua.result+'")').each((inde, el) => {
+            //     let text = el.innerHTML;
+            //     text = text.replace(new RegExp(ketqua.result, 'gmi'), htmlReplace);
+            //     el.innerHTML = text;
+            // })
+            alert(ketqua?.result);
         } else {
             alert('Không có kết quả');
         }
--- tim-hieu-phap-luat/js/libs/helperFunc.js
+++ tim-hieu-phap-luat/js/libs/helperFunc.js
@@ -198,7 +198,166 @@ const questions = [
     {
         question: "Theo Nghị định số 61/2023/NĐ-CP ngày 16/8/2023 của Chính phủ, cơ quan, cá nhân nào dưới đây có thẩm quyền công nhận hương ước, quy ước?",
         result: "Ủy ban nhân dân cấp xã."
+    },
+    {
+        question: "Theo Luật Thực hiện dân chủ ở cơ sở, nội dung nào sau đây là một trong những trách nhiệm của Ban lãnh đạo doanh nghiệp nhà nước trong việc bảo đảm để người lao động thực hiện kiểm tra, giám sát?",
+        result: "Phối hợp cùng Ban Chấp hành Công đoàn ở doanh nghiệp, ban đại diện của tổ chức đại diện khác của người lao động ở cơ sở (nếu có), căn cứ vào yêu cầu, nhiệm vụ, đặc điểm, tính chất tổ chức, hoạt động và điều kiện thực tế của doanh nghiệp, xây dựng và ban hành quy chế thực hiện dân chủ để quy định cụ thể hơn nội dung, cách thức thực hiện dân chủ ở doanh nghiệp mình làm cơ sở để người lao động kiểm tra, giám sát việc thực hiện. Nội dung của quy chế thực hiện dân chủ của doanh nghiệp có thể mở rộng hơn phạm vi thực hiện dân chủ của người lao động nhưng không được trái hoặc hạn chế việc thực hiện các nội dung đã được quy định trong Luật Thực hiện dân chủ ở cơ sở."
+    },
+    {
+        question: "Theo Luật Thực hiện dân chủ ở cơ sở, nhiệm vụ, quyền hạn của Ban Thanh tra nhân dân ở cơ quan, đơn vị được quy định như thế nào?",
+        result: "Tất cả các đáp án."
+    },
+    {
+        question: "Theo Luật Thực hiện dân chủ ở cơ sở, cán bộ, công chức, viên chức, người lao động giám sát nội dung nào?",
+        result: "Cán bộ, công chức, viên chức, người lao động giám sát việc tổ chức thực hiện dân chủ ở cơ sở và việc thực hiện chính sách, pháp luật của người đứng đầu, ban lãnh đạo, người có thẩm quyền của cơ quan, đơn vị, hành vi hành chính của người thực thi công vụ, nhiệm vụ tại cơ quan, đơn vị."
+    },
+    {
+        question: "Theo Luật Thực hiện dân chủ ở cơ sở, trách nhiệm của người đứng đầu cơ quan, đơn vị trong việc tổ chức để cán bộ, công chức, viên chức, người lao động bàn, quyết định và thực hiện quyết định của tập thể cán bộ, công chức, viên chức, người lao động như thế nào?",
+        result: "Tất cả các đáp án."
+    },
+    {
+        question: "Theo Luật Thực hiện dân chủ ở cơ sở, khẳng định nào sau đây là đúng về “Đại diện hộ gia đình”?",
+        result: "Là người có năng lực hành vi dân sự đầy đủ, có khả năng đại diện được cho các thành viên hộ gia đình."
+    },
+    {
+        question: "Theo Luật Thực hiện dân chủ ở cơ sở, quyết định của cộng đồng dân cư về việc thu, chi, quản lý các khoản đóng góp của Nhân dân tại cộng đồng dân cư ngoài các khoản đã được pháp luật quy định có phạm vi thực hiện trong địa bàn thôn, tổ dân phố được thông qua khi nào?",
+        result: "Khi có từ hai phần ba tổng số đại diện hộ gia đình trở lên trong thôn, tổ dân phố tán thành."
+    },
+    {
+        question: "Theo Nghị định số 61/2023/NĐ-CP ngày 16/8/2023 của Chính phủ, Trưởng thôn, Tổ trưởng tổ dân phố có trách nhiệm như thế nào trong xây dựng và thực hiện hương ước, quy ước?",
+        result: "Tổ chức thực hiện hương ước, quy ước của cộng đồng dân cư sau khi được Ủy ban nhân dân cấp xã công nhận."
+    },
+    {
+        question: "Theo Luật Thực hiện dân chủ ở cơ sở, hình thức công khai thông tin ở cơ quan, đơn vị được quy định như thế nào?",
+        result: "Tất cả các đáp án."
+    },
+    {
+        question: "Theo Luật Thực hiện dân chủ ở cơ sở, nội dung nào sau đây được xác định là quyền thụ hưởng của công dân?",
+        result: "Được thụ hưởng thành quả đổi mới, phát triển kinh tế - xã hội, chế độ an sinh xã hội, sự an toàn, ổn định của đất nước, của địa phương, cơ quan, đơn vị, tổ chức có sử dụng lao động và kết quả thực hiện dân chủ ở nơi mình cư trú, công tác, làm việc."
+    },
+    {
+        question: "Theo Nghị định số 59/2023/NĐ-CP ngày 14/8/2023 của Chính phủ, kế hoạch tổ chức hội nghị định kỳ của cộng đồng dân cư được gửi đến đối tượng nào dưới đây để báo cáo, thống nhất trước khi thực hiện?",
+        result: "Ủy ban nhân dân cấp xã."
+    },
+    {
+        question: "Theo Luật Thực hiện dân chủ ở cơ sở, cơ quan, cá nhân nào dưới đây có thẩm quyền quyết định lựa chọn một hoặc một số hình thức tổ chức lấy ý kiến Nhân dân?",
+        result: "Ủy ban nhân dân cấp xã."
+    },
+    {
+        question: "Theo Luật Thực hiện dân chủ ở cơ sở, đâu là trách nhiệm của Ủy ban nhân dân cấp xã trong việc bảo đảm hoạt động của Ban Giám sát đầu tư của cộng đồng?",
+        result: "Tất cả các đáp án."
+    },
+    {
+        question: "Theo Luật Thực hiện dân chủ ở cơ sở, việc xử lý vi phạm pháp luật về thực hiện dân chủ ở cơ sở đối với tổ chức được quy định như thế nào?",
+        result: "Tổ chức vi phạm quy định của Luật Thực hiện dân chủ ở cơ sở và quy định khác của pháp luật có liên quan đến thực hiện dân chủ ở cơ sở thì tùy theo tính chất, mức độ vi phạm mà bị xử phạt vi phạm hành chính; nếu gây thiệt hại thì phải bồi thường theo quy định của pháp luật."
+    },
+    {
+        question: "Luật Thực hiện dân chủ ở cơ sở quy định khi nào thì Hội nghị người lao động được tổ chức bất thường?",
+        result: "Hội nghị người lao động được tổ chức bất thường khi có ít nhất là một phần ba tổng số người lao động của doanh nghiệp cùng đề nghị liên quan đến việc lập các loại quỹ và thu, chi, quản lý, sử dụng các khoản từ thu nhập, đóng góp của người lao động."
+    },
+    {
+        question: "Theo Luật Thực hiện dân chủ ở cơ sở, ý kiến đồng thuận với sáng kiến của công dân có giá trị trong thời hạn bao lâu kể từ ngày thể hiện ý kiến?",
+        result: "03 tháng."
+    },
+    {
+        question: "Theo Nghị định số 59/2023/NĐ-CP ngày 14/8/2023 của Chính phủ, cơ quan, cá nhân nào dưới đây có thẩm quyền quyết định số lượng Phó Trưởng ban Thanh tra nhân dân ở xã, phường, thị trấn?",
+        result: "Ủy ban Mặt trận Tổ quốc Việt Nam cấp xã."
+    },
+    {
+        question: "Theo Nghị định số 61/2023/NĐ-CP ngày 16/8/2023 của Chính phủ, trường hợp nào dưới đây, hương ước, quy ước cần được sửa đổi, bổ sung, thay thế?",
+        result: "Tất cả các đáp án."
+    },
+    {
+        question: "Theo Luật Thực hiện dân chủ ở cơ sở, Ban Thanh tra nhân dân ở cơ quan, đơn vị gồm bao nhiêu thành viên?",
+        result: "Ban Thanh tra nhân dân ở cơ quan, đơn vị do hội nghị cán bộ, công chức, viên chức, người lao động bầu gồm từ 03 đến 09 thành viên theo đề nghị của Ban Chấp hành Công đoàn cơ quan, đơn vị."
+    },
+    {
+        question: "Luật Thực hiện dân chủ ở cơ sở quy định như thế nào về trách nhiệm của Ban Chấp hành Công đoàn ở doanh nghiệp, tổ chức đại diện khác của người lao động ở cơ sở trong việc tổ chức để người lao động tham gia ý kiến",
+        result: "Ban Chấp hành Công đoàn ở doanh nghiệp, tổ chức đại diện khác của người lao động ở cơ sở (nếu có) tham gia phối hợp thực hiện việc tổ chức lấy ý kiến; giám sát việc lấy ý kiến của người lao động, kết quả giải trình, tiếp thu và tổ chức thực hiện các nội dung người lao động đã tham gia ý kiến; tham gia ý kiến trước khi doanh nghiệp quyết định ban hành quy chế thực hiện dân chủ của doanh nghiệp."
+    },
+    {
+        question: "Theo Luật Thực hiện dân chủ ở cơ sở, nội dung nào sau đây được Nhân dân bàn và quyết định?",
+        result: "Việc thu, chi, quản lý các khoản đóng góp của Nhân dân tại cộng đồng dân cư ngoài các khoản đã được pháp luật quy định; việc thu, chi, quản lý các khoản kinh phí, tài sản do cộng đồng dân cư được giao quản lý hoặc được tiếp nhận từ các nguồn thu, tài trợ, ủng hộ hợp pháp khác."
+    },
+    {
+        question: "Theo Nghị định số 61/2023/NĐ-CP ngày 16/8/2023 của Chính phủ, thời hạn để Trưởng thôn hoặc Tổ trưởng tổ dân phố gửi hồ sơ đề nghị công nhận hương ước, quy ước tới Ủy ban nhân dân cấp xã là?",
+        result: "Chậm nhất sau 05 ngày làm việc, kể từ ngày tổ chức cuộc họp cộng đồng dân cư thông qua hương ước, quy ước hoặc ngày kết thúc thời hạn lấy ý kiến bằng phiếu."
+    },
+
+    {
+        question: "Việc áp dụng pháp luật và quy định chuyển tiếp khi thực hiện Luật thực hiện dân chủ ở cơ sở được quy định như thế nào?",
+        result: "Tất cả các đáp án."
+    },
+    {
+        question: "Theo Luật Thực hiện dân chủ ở cơ sở, đâu là nhiệm vụ của Ban Giám sát đầu tư của cộng đồng?",
+        result: "Phát hiện những việc làm tổn hại đến lợi ích của cộng đồng; những tác động tiêu cực của dự án đến môi trường sinh sống của cộng đồng trong quá trình thực hiện đầu tư và vận hành dự án; những việc gây lãng phí, thất thoát vốn, tài sản thuộc các chương trình, dự án đầu tư."
+    },
+    {
+        question: "Theo Luật Thực hiện dân chủ ở cơ sở, đâu là trách nhiệm của Nhân dân trong việc tham gia ý kiến về các nội dung ở xã, phường, thị trấn?",
+        result: "Cá nhân, hộ gia đình có trách nhiệm tham gia hoặc cử đại diện hộ gia đình tham dự họp bàn, thảo luận, thể hiện ý kiến đối với các nội dung theo yêu cầu của cấp có thẩm quyền."
+    },
+    {
+        question: "Theo Nghị định số 61/2023/NĐ-CP ngày 16/8/2023 của Chính phủ, đâu là nguyên tắc xây dựng và thực hiện hương ước, quy ước?",
+        result: "Phù hợp với đạo đức xã hội, phong tục, tập quán tốt đẹp; bảo vệ, giữ gìn, phát huy các giá trị văn hóa truyền thống; xây dựng các giá trị văn hóa mới, quy tắc ứng xử văn minh, phù hợp với đặc điểm tình hình của cộng đồng dân cư."
+    },
+    {
+        question: "Theo Luật Thực hiện dân chủ ở cơ sở, nội dung nào người lao động tham gia ý kiến theo đề nghị của tổ chức đại diện người lao động?",
+        result: "Nội dung thỏa ước lao động tập thể và đối thoại tại doanh nghiệp nhà nước."
+    },
+    {
+        question: "Theo Nghị định số 61/2023/NĐ-CP ngày 16/8/2023 của Chính phủ, trách nhiệm của Ủy ban nhân dân cấp tỉnh trong xây dựng và thực hiện hương ước, quy ước của cộng đồng dân cư được quy định như thế nào?",
+        result: "Tất cả các đáp án."
+    },
+    {
+        question: "Theo Nghị định số 59/2023/NĐ-CP ngày 14/8/2023 của Chính phủ, nội dung nào sau đây là phương thức hoạt động của Ban Thanh tra nhân dân ở cơ quan, đơn vị?",
+        result: "Kết quả kiểm tra, giám sát, xác minh được lập thành văn bản và gửi đến cơ quan, tổ chức, các nhân có phản ánh, kiến nghị. Trường hợp phát hiện người có hành vi vi phạm pháp luật thì kiến nghị cơ quan, tổ chức có thẩm quyền xem xét, xử lý theo quy định."
+    },
+    {
+        question: "Theo Nghị định số 59/2023/NĐ-CP ngày 14/8/2023 của Chính phủ, số lượng thành viên Ban Giám sát đầu tư của cộng đồng là đại diện người dân trên địa bàn thôn, tổ dân phố nơi có chương trình, dự án do cơ quan nào quyết định?",
+        result: "Do Ban Thường trực Ủy ban Mặt trận Tổ quốc Việt Nam cấp xã quyết định từ 05 - 07 người."
+    },
+    {
+        question: "Theo Luật Thực hiện dân chủ ở cơ sở, thời điểm công khai thông tin đối với những lĩnh vực mà pháp luật liên quan chưa có quy định là?",
+        result: "Chậm nhất là 10 ngày làm việc kể từ ngày có quyết định, văn bản của cơ quan có thẩm quyền về nội dung cần công khai."
+    },
+    {
+        question: "Theo Luật Thực hiện dân chủ ở cơ sở, tại nơi không có đơn vị hành chính cấp xã thì cơ quan nào có trách nhiệm tổ chức thực hiện pháp luật về thực hiện dân chủ ở cơ sở?",
+        result: "Hội đồng nhân dân và Ủy ban nhân dân cấp huyện."
+    },
+    {
+        question: "Theo Luật Thực hiện dân chủ ở cơ sở, thông qua việc kiểm tra, giám sát, công dân có quyền nào dưới đây khi phát hiện ra hành vi, nội dung có dấu hiệu vi phạm?",
+        result: "Tất cả các đáp án."
+    },
+    {
+        question: "Câu hỏi số 36: Theo Luật Thực hiện dân chủ ở cơ sở, cán bộ, công chức, viên chức, người lao động bàn và quyết định những nội dung nào sau đây?",
+        result: "Tất cả các đáp án."
+    },
+    {
+        question: "Theo Nghị định số 61/2023/NĐ-CP ngày 16/8/2023 của Chính phủ, hương ước, quy ước được thông qua khi:",
+        result: "Có trên 50% tổng số đại diện hộ gia đình trong cộng đồng dân cư tán thành."
+    },
+    {
+        question: "Theo Luật Thực hiện dân chủ ở cơ sở, đâu là trách nhiệm của Ủy ban Mặt trận Tổ quốc Việt Nam cấp xã trong việc bảo đảm hoạt động của Ban Thanh tra nhân dân ở xã, phường, thị trấn?",
+        result: "Hỗ trợ kinh phí hoạt động cho Ban Thanh tra nhân dân. Kinh phí hỗ trợ cho hoạt động của Ban Thanh tra nhân dân được sử dụng từ nguồn ngân sách nhà nước theo dự toán, kế hoạch hằng năm của Ủy ban Mặt trận Tổ quốc Việt Nam cấp xã, do ngân sách nhà nước cấp xã bảo đảm."
+    },
+    {
+        question: "Luật Thực hiện dân chủ ở cơ sở quy định như thế nào về thời điểm công khai thông tin ở doanh nghiệp nhà nước?",
+        result: "Chậm nhất là 15 ngày kể từ ngày có quyết định, văn bản của người có thẩm quyền về nội dung cần công khai, trừ trường hợp pháp luật có quy định khác."
+    },
+    {
+        question: "Theo Nghị định số 61/2023/NĐ-CP ngày 16/8/2023 của Chính phủ, kinh phí thực hiện được quy định như thế nào?",
+        result: "Tất cả các đáp án."
+    },
+    {
+        question: "Theo Luật Thực hiện dân chủ ở cơ sở, người đứng đầu cơ quan, đơn vị có trách nhiệm như thế nào trong việc bảo đảm để cán bộ, công chức, viên chức, người lao động thực hiện việc kiểm tra, giám sát?",
+        result: "Thực hiện các biện pháp phòng ngừa, ngăn chặn hành vi tham nhũng, tiêu cực; xử lý và tạo điều kiện để cơ quan, tổ chức có thẩm quyền xử lý người có hành vi tham nhũng, tiêu cực; nếu thiếu trách nhiệm để xảy ra tham nhũng, tiêu cực trong cơ quan, đơn vị thì bị xử lý theo quy định của pháp luật."
+    },
+    {
+        question: "Cơ cấu, trách nhiệm của Ban Thanh tra nhân dân ở doanh nghiệp nhà nước được Luật Thực hiện dân chủ ở cơ sở quy định thế nào?",
+        result: "Gồm trưởng ban, phó ban và các ủy viên. Trưởng ban chịu trách nhiệm chung về HĐ của ban TTND, phó trưởng ban chịu trách nhiệm giúp TB..., các thành viên khác thực hiện nhiệm vụ theo sự phân công."
     }
+
+    
 ];
 
 function removeVietnameseTones(str) {
